73abd73930a90c7a4b12f3b057e0c6d48dd42f3d
danilo duarte <82615421+danako24@users.noreply.github.com>
2024-09-11 13:07:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 let nombre = "valentina"
 let apellido = "cid"
 let años = 3
-
+let vives= "santiago"
+let gato = "tomi"
 
 let nombre_usuario = prompt('nombre y apellido de mi amorcito').toLowerCase()
 
@@ -10,19 +11,36 @@ let nombre_usuario = prompt('nombre y apellido de mi amorcito').toLowerCase()
 if (nombre + " " + apellido == nombre_usuario){
     alert('si eres mi amorcito <3')
 
-    let años_juntos= Number(prompt('cuantos años juntos?'))
-    
-    if (años == años_juntos){
-        alert('siiiiii')
-    }
-    else{
-        alert('mmmmmmm')
-    }
-
-
 } else {
     alert('no eres 7.7')
 }
+
+let años_juntos= Number(prompt('cuantos años juntos?'))
+    
+if (años == años_juntos){
+    alert('siiiiii')
+}
+else{
+    alert('mmmmmmm')
+}
+
+let lugar = prompt('ciudad donde vives?').toLowerCase()
+
+if (vives == lugar){
+    alert('pues si donde mas')
+} else{
+    alert('ñao')
+}
+
+let mascota = prompt('nombre de tu gatito').toLowerCase()
+
+if (gato == mascota){
+    alert('y la ivy? 7.7')
+} else{
+    alert(':c')
+}
+
+
 
 
 console.log('chao')
