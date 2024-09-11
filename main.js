bb1ceef7b85b2083d8bb75a80820444e3c6e3a3d
danilo duarte <82615421+danako24@users.noreply.github.com>
2024-09-11 12:48:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 
 let nombre = "valentina"
 let apellido = "cid"
+let años = 3
 
 
 let nombre_usuario = prompt('nombre y apellido de mi amorcito').toLowerCase()
 
+
 if (nombre + " " + apellido == nombre_usuario){
     alert('si eres mi amorcito <3')
+
+    let años_juntos= Number(prompt('cuantos años juntos?'))
+    
+    if (años == años_juntos){
+        alert('siiiiii')
+    }
+    else{
+        alert('mmmmmmm')
+    }
 
 
 } else {
